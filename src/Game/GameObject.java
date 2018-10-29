6b4e92b412b459277e227d553b9f2ded324190f3
league <league@wts8.attlocal.net>
2018-10-28 17:30:11 -0700

    movement and jumping

--- src/Game/GameObject.java
+++ src/Game/GameObject.java
@@ -5,38 +5,61 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 public class GameObject {
-int x;
-int y;
-int width;
-int height;
-int speed = 5;
-Rectangle collisionBox;
+	int x;
+	int y;
+	int width;
+	int height;
+	int gravity = 1;
+	int speed = 5;
+	int velocity = 20;
+	Rectangle collisionBox;
+	boolean left, right, jump, air;
 
-GameObject(int a, int b , int c ,int d)
-{
-x = a;
-y = b;
-width = c;
-height = d;
-collisionBox = new Rectangle(x,y,width,height);
-}
-void update()
-{
-collisionBox.setBounds(x, y, width, height);	
-}
-void draw(Graphics g)
-{
-g.setColor(new Color(255,0,0));
-g.fillRect(x, y, width, height);
-}
-void l()
-{
-x-=speed;	
-}
-void r()
-{
-x+=speed;	
-}
+	GameObject(int a, int b, int c, int d) {
+		x = a;
+		y = b;
+		width = c;
+		height = d;
+		collisionBox = new Rectangle(x, y, width, height);
 
+	}
+
+	void update() {
+		if (left == true) {
+			l();
+		}
+
+		if (right == true) {
+			r();
+		}
+
+		velocity += gravity;
+		y += velocity;
+
+		if (y >= 501) {
+			y = 501;
+			velocity = 0;
+		}
+
+		collisionBox.setBounds(x, y, width, height);
+	}
+
+	void draw(Graphics g) {
+		g.setColor(new Color(255, 0, 0));
+		g.fillRect(x, y, width, height);
+	}
+
+	void jump() {
+		velocity = -10;
+	}
+
+	void l() {
+		x -= speed;
+		jump = false;
+	}
+
+	void r() {
+		x += speed;
+	}
 
 }
--- src/Game/GamePanel.java
+++ src/Game/GamePanel.java
@@ -16,7 +16,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener, Mo
 	Timer tim;
 
 	GamePanel() {
-		go = new GameObject(20, 20, 20, 20);
+		go = new GameObject(200, 200, 20, 20);
 		tim = new Timer(1000 / 60, this);
 		tim.start();
 	}
@@ -63,33 +63,42 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener, Mo
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-char key = e.getKeyChar();
-		// TODO Auto-generated method stub
-		if (key == 'a')
-		{
-		go.l();	
-		}
-		if (key == 'd')
-		{
-		go.r();	
-		}
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		char key = e.getKeyChar();
+		// TODO Auto-generated method stub
+		if (key == 'a') {
+			go.left = true;
+		}
+		if (key == 'd') {
+			go.right = true;
+		}
+		if (key == ' ') {
+				go.jump();
+		}
 
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		char key = e.getKeyChar();
+		if (key == 'a') {
+			go.left = false;
+		}
+		if (key == 'd') {
+			go.right = false;
+		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		go.update();
 		repaint();
 	}
 }
